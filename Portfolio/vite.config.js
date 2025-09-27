c9d8779f6adc6be78edcf814aad8e8941d8808d9
kat-in <newkatelife@gmail.com>
2025-09-27 16:01:29 +0300

    Deploy build from Portfolio

--- Portfolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  build: {
-    sourcemap: true,   // включаем source maps
-    minify: false,     // отключаем минификацию
-    outDir: 'dist',    // папка сборки
-  }
-})
