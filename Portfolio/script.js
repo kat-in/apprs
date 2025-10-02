2f0468bb9c51a9f47f71c0cf1859639cdad6e4ac
kat-in <newkatelife@gmail.com>
2025-10-02 09:51:37 +0300

    refactor: burger menu

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -6,15 +6,44 @@ const state = {
 
 const hamburger = document.querySelector('.hamburger')
 const nav = document.querySelector('.hamburger__menu')
+const nav_items = nav.querySelectorAll('li')
 
 hamburger.addEventListener('click', (e) => {
-    nav.setAttribute('id','show_nav')
-    hamburger.classList.add('cross')
-    const firstLine = hamburger.querySelector('.hamburger_small_line');
-    firstLine.classList.add('line_rigth');
-    firstLine.classList.remove('hamburger_small_line')
-    const secondLine = hamburger.querySelector('.hamburger_line');
-    secondLine.classList.add('line_left');
-    secondLine.classList.remove('hamburger_line')
-    document.body.classList.add('fixed');
+    if (nav.getAttribute('id') === 'hide') {
+
+        nav.setAttribute('id', 'show')
+        hamburger.classList.add('cross')
+
+        hamburger.firstElementChild.classList.add('line_rigth')
+        hamburger.firstElementChild.classList.remove('hamburger_small_line')
+        hamburger.lastElementChild.classList.add('line_left');
+        hamburger.lastElementChild.classList.remove('hamburger_line')
+
+        document.body.classList.add('fixed');
+    }
+    else if (nav.getAttribute('id') === 'show') {
+        nav.setAttribute('id', 'hide')
+        hamburger.classList?.remove('cross')
+
+        hamburger.firstElementChild.classList.add('hamburger_small_line')
+        hamburger.firstElementChild.classList.remove('line_rigth')
+        hamburger.lastElementChild.classList.add('hamburger_line');
+        hamburger.lastElementChild.classList.remove('line_left')
+
+        document.body.classList.remove('fixed');
+    }
+})
+
+nav_items.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        nav.setAttribute('id', 'hide')
+        hamburger.classList?.remove('cross')
+
+        hamburger.firstElementChild.classList.add('hamburger_small_line')
+        hamburger.firstElementChild.classList.remove('line_rigth')
+        hamburger.lastElementChild.classList.add('hamburger_line');
+        hamburger.lastElementChild.classList.remove('line_left')
+
+        document.body.classList.remove('fixed');
+    })
 })
